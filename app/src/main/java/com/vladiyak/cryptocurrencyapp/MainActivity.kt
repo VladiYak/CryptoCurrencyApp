d19b8cc53de0c32e8e7a195fc96389b13a99df9f
VladiYak95 <vladimiriakovlev95@gmail.com>
2023-10-19 18:18:24 +0900

    Start

---
+++ app/src/main/java/com/vladiyak/cryptocurrencyapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vladiyak.cryptocurrencyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
