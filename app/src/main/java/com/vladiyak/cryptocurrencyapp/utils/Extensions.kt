7add6e3bd5bafc195ff04ea3ff75a07abc7cfae0
VladiYak95 <vladimiriakovlev95@gmail.com>
2023-11-10 19:23:09 +0900

    fixed bug with chipgroup

--- app/src/main/java/com/vladiyak/cryptocurrencyapp/utils/Extensions.kt
+++ app/src/main/java/com/vladiyak/cryptocurrencyapp/utils/Extensions.kt
@@ -11,11 +11,3 @@ fun Double.format(digits: Int) = "%.${digits}f".format(this)
 fun String.addPrefix(value: String) = this + value
 
 fun String.addSuffix(value: String) = value + this
-
-fun <T : Parcelable?> Bundle?.getParcelableArg(name: String, clazz: Class<T>): T?
-{
-    return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
-        this!!.getParcelable(name, clazz)
-    else
-        this!!.getParcelable(name) as T?
-}
